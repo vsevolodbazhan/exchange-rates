2a7601445a91d5181fb5325235a65e78f8d126a4
Vsevolod Mycroft <mycroft@deeplay.io>
2021-04-21 22:01:28 +1000

    Get exchange rates page contents

--- src/extractor.py
+++ src/extractor.py
@@ -6,6 +6,8 @@ import requests
 
 __all__ = ["ExchangeRate", "extract_exchange_rates"]
 
+BASE_URL = "http://cbr.ru/scripts/XML_daily.asp?date_req={date}"
+
 
 @dataclass
 class ExchangeRate:
@@ -14,7 +16,14 @@ class ExchangeRate:
     rate: float
 
 
+def _get_exchange_rates_markup(base_url: str, date: str) -> str:
+    response = requests.get(url=base_url.format(date=date))
+    response.raise_for_status()
+    return response.text
+
+
 def extract_exchange_rates(period: pendulum.period) -> List[ExchangeRate]:
     for date in period:
-        date = date.strftime("%d.%m.%Y")
-        print(date)
+        date = date.strftime("%d/%m/%Y")
+        markup = _get_exchange_rates_markup(base_url=BASE_URL, date=date)
+        print(markup)
