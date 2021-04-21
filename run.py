fe6b1fc0a941edc07c8c271e25c8de704d331b2a
Vsevolod Mycroft <mycroft@deeplay.io>
2021-04-21 21:53:59 +1000

    Init `extractor` module

---
+++ run.py
@@ -0,0 +1,12 @@
+import pendulum
+
+from src.extractor import extract_exchange_rates
+
+
+if __name__ == "__main__":
+    extract_exchange_rates = extract_exchange_rates(
+        period=pendulum.period(
+            start=pendulum.datetime(year=2019, month=12, day=1),
+            end=pendulum.datetime(year=2020, month=8, day=1),
+        )
+    )
---
+++ src/extractor.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import List
+
+import pendulum
+import requests
+
+__all__ = ["ExchangeRate", "extract_exchange_rates"]
+
+
+@dataclass
+class ExchangeRate:
+    date: str
+    currency_code: str
+    rate: float
+
+
+def extract_exchange_rates(period: pendulum.period) -> List[ExchangeRate]:
+    for date in period:
+        print(date)
